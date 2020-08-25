0e1d58e142110362e3268dd4b131f8a60f78e3fc
JS <jinseoit@gmail.com>
2020-08-25 22:35:56 +0900

    for문 1개

--- Algorithm_Java/Algorithm_Java/src/Chapter3_for/for_03.java
+++ Algorithm_Java/Algorithm_Java/src/Chapter3_for/for_03.java
@@ -1,10 +1,20 @@
 package Chapter3_for;
 
+import java.util.Scanner;
+
 public class for_03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int num  = sc.nextInt();
+		int result = 0;
+		for(int i=1;i<=num;i++) {
+			result += i;
+		}
+		
+		System.out.println(result);
+		
 	}
 
 }
