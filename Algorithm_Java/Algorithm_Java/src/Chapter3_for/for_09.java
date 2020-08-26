df1c65f8aa775e1b9f8aaf108b78eb1cf0e5aea4
JS <jinseoit@gmail.com>
2020-08-26 23:57:14 +0900

    for_09

---
+++ Algorithm_Java/Algorithm_Java/src/Chapter3_for/for_09.java
@@ -0,0 +1,20 @@
+package Chapter3_for;
+
+import java.util.Scanner;
+
+public class for_09 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		for (int i = 1; i <= num; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+		
+	}
+
+}
